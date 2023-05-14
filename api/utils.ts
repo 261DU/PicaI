d0b52c1ff3070f6d18efd8396c3b29eb4345066f
Yujio_Nako <69411867+ldcivan@users.noreply.github.com>
2023-05-15 01:16:02 +0800

    Update utils.ts

--- api/utils.ts
+++ api/utils.ts
@@ -40,8 +40,8 @@ export function replaceFileUrl(obj: Record<string, any>) {
     if (typeof val === 'string') {
       if (val.startsWith('https://'||'http://')) {
         obj[key] = val
-          .replace(`storage1.picacomic.com`, `pikaimg.pro-ivan.com`)
           .replace('img.picacomic.com', 'pikas3.pro-ivan.com')
+          .replace(`storage1.picacomic.com`, `pikaimg.pro-ivan.com`)
           .replace('www.picacomic.com', 'pica-pica.wikawika.xyz')
       }
     }
