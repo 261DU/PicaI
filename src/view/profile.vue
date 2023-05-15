3044c104a8a0c9de5bf823622d6a9ef1428e176f
Yujio_Nako <69411867+ldcivan@users.noreply.github.com>
2023-05-15 14:25:23 +0800

    Update profile.vue

--- src/view/profile.vue
+++ src/view/profile.vue
@@ -4,7 +4,7 @@
     //- h1 My Profile
     .card.metadata.align-center
       .avatar
-        img(src='https://i.loli.net/2021/03/26/QPOtzh1XbF2eujd.png')
+        img(src= userData.fileUrl onerror= "this.src='https://i.loli.net/2021/03/26/QPOtzh1XbF2eujd.png'")
       h1.name {{ userData.name }}
       .extra
         span.title {{ userData.title }}
