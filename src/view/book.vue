8b33a18195d805569001356529fb65c377e19814
Yujio_Nako <69411867+ldcivan@users.noreply.github.com>
2023-07-06 23:28:29 +0800

    Update book.vue

--- src/view/book.vue
+++ src/view/book.vue
@@ -85,7 +85,7 @@
         .pages(v-for='item in comments') 
           strong {{ item._user.name }}:
           | {{ item.content }}    At {{ item.created_at }}
-        lable(v-if="comments.body.comments.page < comments.body.comments.pages || commentsLoading || !comments.length")
+        lable(v-if="commentsLoading || !comments.length || comments.body.comments.page < comments.body.comments.pages")
           button(@click='getComments(parseInt(comments.body.comments.page) + 1)') Load more...
       details
         pre {{ comments }}
